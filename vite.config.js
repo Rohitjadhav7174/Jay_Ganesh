fcc3f26d377010d8f2d1353fdd03acd38cabcb19
Rohit Jadhav <113785547+Rohitjadhav7174@users.noreply.github.com>
2025-10-28 15:13:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://jay-ganesh-backend.vercel.app/', // Replace with your Vercel backend URL
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
